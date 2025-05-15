8a6885fc1ec0a51d652e951ceeb767523bb8930b
Simeon Goranov <sgoranov@users.noreply.github.com>
2025-05-15 14:56:20 +0300

    Remove unnecessary dependency

--- src/Controller/PingController.php
+++ src/Controller/PingController.php
@@ -3,13 +3,12 @@ declare(strict_types=1);
 
 namespace sgoranov\PHPIdentityLinkShared\Controller;
 
-use sgoranov\PHPIdentityLinkShared\Api\DTO\AbstractQueryRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class PingController extends AbstractController
 {
-    public function ping(AbstractQueryRequest $request): Response
+    public function ping(): Response
     {
         return new Response('pong');
     }
